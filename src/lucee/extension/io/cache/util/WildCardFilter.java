6c823b8acac2d1a0414ce86de5ac66a446b46ee4
Thomas Rotter <trotter@idg.de>
2015-07-07 14:07:14 +0200

    spaces -> tabs

--- src/lucee/extension/io/cache/util/WildCardFilter.java
+++ src/lucee/extension/io/cache/util/WildCardFilter.java
@@ -13,34 +13,34 @@ import lucee.commons.io.cache.CacheKeyFilter;
  */
 public class WildCardFilter implements CacheKeyFilter {
 
-    private static final String specials="{}[]().+\\^$";
+	private static final String specials="{}[]().+\\^$";
 
 	private final Pattern pattern;
-    private final PatternMatcher matcher=new Perl5Matcher();
+	private final PatternMatcher matcher=new Perl5Matcher();
 	private final String wildcard;
 
 	private boolean ignoreCase;
 
 
-    /**
-     * @param wildcard
-     * @throws MalformedPatternException
-     */
-    public WildCardFilter(String wildcard,boolean ignoreCase) throws MalformedPatternException {
-        this.wildcard=wildcard;
-        this.ignoreCase=ignoreCase;
-        StringBuffer sb = new StringBuffer(wildcard.length());
-        int len=wildcard.length();
+	/**
+	 * @param wildcard
+	 * @throws MalformedPatternException
+	 */
+	public WildCardFilter(String wildcard,boolean ignoreCase) throws MalformedPatternException {
+		this.wildcard=wildcard;
+		this.ignoreCase=ignoreCase;
+		StringBuffer sb = new StringBuffer(wildcard.length());
+		int len=wildcard.length();
 
-        for(int i=0;i<len;i++) {
-            char c = wildcard.charAt(i);
-            if(c == '*')sb.append(".*");
-            else if(c == '?') sb.append('.');
-            else if(specials.indexOf(c)!=-1)sb.append('\\').append(c);
-            else sb.append(c);
-        }
-        pattern=new Perl5Compiler().compile(ignoreCase?sb.toString().toLowerCase():sb.toString());
-    }
+		for(int i=0;i<len;i++) {
+			char c = wildcard.charAt(i);
+			if(c == '*')sb.append(".*");
+			else if(c == '?') sb.append('.');
+			else if(specials.indexOf(c)!=-1)sb.append('\\').append(c);
+			else sb.append(c);
+		}
+		pattern=new Perl5Compiler().compile(ignoreCase?sb.toString().toLowerCase():sb.toString());
+	}
 
 	/**
 	 * @see lucee.commons.io.cache.CacheKeyFilter#accept(java.lang.String)
@@ -49,7 +49,7 @@ public class WildCardFilter implements CacheKeyFilter {
 		return matcher.matches(ignoreCase?key.toLowerCase():key, pattern);
 	}
 
-    /**
+	/**
 	 *
 	 * @see java.lang.Object#toString()
 	 */
